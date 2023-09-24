741078fa06f7a2155df5298337f8265cce7babe8
Yan Luiz <yan.luiz.lima@gmail.com>
2023-09-23 21:56:34 -0300

    criando css global e utilizando ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details.jsx'
+import { Details } from './pages/Details/index.jsx'
+import { ThemeProvider } from 'styled-components'
 
+import { theme } from '../src/styles/theme.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
-  </React.StrictMode>,
+
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
 )
