317e59fcb1ae1d68a97010744738de3d78812e3c
Yan Luiz <yan.luiz.lima@gmail.com>
2023-09-24 14:42:43 -0300

    criando arquivo global.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details/index.jsx'
 import { ThemeProvider } from 'styled-components'
+import  GlobalStyles  from './styles/global.js'
 
-import { theme } from '../src/styles/theme.js'
+import  theme  from '../src/styles/theme.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <ThemeProvider theme={theme}>
+      <GlobalStyles/>
       <Details />
     </ThemeProvider>
   </React.StrictMode>
