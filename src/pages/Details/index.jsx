a968ce712c958d52649dd147959b261ab85cb39f
Yan Luiz <yan.luiz.lima@gmail.com>
2023-09-30 20:55:56 -0300

    criando componente ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,11 +2,13 @@ import { Container, Links } from './styles.js'
 import { Header } from '../../components/Header/index.jsx'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { ButtonText } from '../../components/ButtonText'
 import { Tag } from '../../components/Tag'
 export function Details() {
     return (
         <Container>
           <Header/>
+          <ButtonText title="excluir nota"/>
           <Section title="Links Úteis">
             <Links>
               <li><a href="https://aprovadoapp.com/"></a>Link 1</li>
