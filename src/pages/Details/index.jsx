876cfc6b48e241f60c65cda99ea2a75a54fad41e
Yan Luiz <yan.luiz.lima@gmail.com>
2023-09-30 21:19:16 -0300

    alinhamento e conclusão pagina de Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 import { Header } from '../../components/Header/index.jsx'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
@@ -6,9 +6,22 @@ import { ButtonText } from '../../components/ButtonText'
 import { Tag } from '../../components/Tag'
 export function Details() {
     return (
-        <Container>
-          <Header/>
+    <Container>
+      <Header/>
+
+        <main>
+         <Content>
+
+
+
+            
           <ButtonText title="excluir nota"/>
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi ipsam nostrum optio ipsa, voluptatibus consequatur nesciunt ipsum vel velit fugiat, tempore accusantium explicabo inventore! Dignissimos architecto nobis enim harum rem.</p>
+
           <Section title="Links Úteis">
             <Links>
               <li><a href="https://aprovadoapp.com/"></a>Link 1</li>
@@ -22,7 +35,10 @@ export function Details() {
           </Section>  
 
           <Button title="Voltar"/>
-        </Container>
+
+        </Content>
+      </main>
+    </Container>
 
     )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -14,7 +14,11 @@ export const Container = styled.div`
     "header" 
     "content";
 
-
+    > main {
+        grid-area: content;
+        overflow-y: auto;
+        padding: 64px 0;
+    }
 `
 
 
@@ -32,4 +36,31 @@ export const Links = styled.ul`
 
     }
 
+`
+
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+    display: flex;
+
+    flex-direction: column;
+
+    > button:nth-child(1) {
+        align-self: end;
+    }
+
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        padding-top: 64px;
+    }
+
+
+    > p {
+        font-size: 16px;
+        margin-top: 16px;
+        text-align: justify;
+    }
 `
