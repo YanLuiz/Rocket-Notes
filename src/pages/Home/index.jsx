b341e51105b52258c8e3dd99bac3e782aec003b2
Yan Luiz <yan.luiz.lima@gmail.com>
2023-10-01 17:05:35 -0300

    criando componente inpit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
-import { FiPlus } from 'react-icons/fi'
-
+import { FiPlus, FiSearch } from 'react-icons/fi'
+import { Input } from '../../components/Input'
 import { Container, Brand,  Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header'
 import { ButtonText } from '../../components/ButtonText'
@@ -23,7 +23,7 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch}/>
             </Search>
             <Content>
 
