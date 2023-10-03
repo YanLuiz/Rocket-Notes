d544509868c91985d7fffb5f67ff25a9feb1808e
Yan Luiz <yan.luiz.lima@gmail.com>
2023-10-02 23:42:46 -0300

    criando a página de login

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home/index.jsx'
 import { ThemeProvider } from 'styled-components'
 import  GlobalStyles  from './styles/global.js'
 
 import  theme  from '../src/styles/theme.js'
+import { SignIn } from './pages/SignIn/index.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,41 @@
+import { Container, Form, Background } from './styles'
+import {Input} from '../../components/Input'
+import {Button} from '../../components/Button'
+import {ButtonText} from '../../components/ButtonText'
+import {FiMail, FiLock} from 'react-icons/fi';
+
+export function SignIn() {
+    return (
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <section>
+                <Button title="Entrar"/>
+                <ButtonText title="Criar Conta"/>
+                
+                </section>
+                
+
+                
+
+            </Form>
+
+            <Background/>
+        </Container>
+    )
+}
