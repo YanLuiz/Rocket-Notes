757f1aea35fe508d5fc84e02d47f42bb29e0e3cc
Yan Luiz <yan.luiz.lima@gmail.com>
2023-10-05 22:28:01 -0300

    iniciando página New e criando componente Textarea

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from 'styled-components'
 import  GlobalStyles  from './styles/global.js'
 
 import  theme  from '../src/styles/theme.js'
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
